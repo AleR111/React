33c8069f18a0b67be05e0e060cb8d1089049e234
Al <alexseier12345@gmail..com>
2021-08-05 20:12:04 +0300

    done chats list

--- my-react-app/src/components/chats/Chats.js
+++ my-react-app/src/components/chats/Chats.js
@@ -1,6 +1,35 @@
-export const Chats = () => {
+import {
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  Avatar,
+} from "@material-ui/core"
+import { useState } from "react"
 
-    return (
-        <h4>Chats list</h4>
-    )
+export const Chats = () => {
+  const [chatsList] = useState([
+    { id: 123, name: "Keeley Lon" },
+    { id: 235, name: "Angelle Jonty" },
+    { id: 543, name: "Myra Justy" },
+    { id: 563, name: "Dwain Bette" },
+    { id: 432, name: "Brenton Oli" },
+  ])
+
+  const getAvatar = (name) => {
+    return name.match(/[A-Z]/g).join("")
+  }
+
+  return (
+    <List component="nav" aria-label="contacts">
+      {chatsList.map((elem) => (
+        <ListItem button={true} key={elem.id}>
+          <ListItemIcon>
+            <Avatar>{getAvatar(elem.name)}</Avatar>
+          </ListItemIcon>
+          <ListItemText primary={elem.name} />
+        </ListItem>
+      ))}
+    </List>
+  )
 }
