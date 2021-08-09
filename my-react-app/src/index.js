2c72e71b16bca948ee1a6f7e771c42401212077c
Al <alexseier12345@gmail..com>
2021-08-09 23:41:53 +0300

    delete app

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,12 +1,34 @@
+import { createTheme, ThemeProvider } from "@material-ui/core/styles"
 import React from "react"
 import ReactDOM from "react-dom"
-
 import "./index.css"
-import { App } from "./App"
+import { Layout, Header, Chats, Messages } from "./components"
+
+const themes = {
+  dark: createTheme({
+    font: {
+      color: "rgba(255,255,255,0.89)",
+    },
+    background: {
+      color: "#19181f",
+    },
+  }),
+
+  light: createTheme({
+    font: {
+      color: "rgba(0,0,0,0.89)",
+    },
+    background: {
+      color: "#ffffff",
+    },
+  }),
+}
 
 ReactDOM.render(
   <React.StrictMode>
-      <App />
+    <ThemeProvider theme={themes.dark}>
+      <Layout Header={<Header />} Chats={<Chats />} Messages={<Messages />} />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root"),
 )
