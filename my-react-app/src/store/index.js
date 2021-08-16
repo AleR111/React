8b0ad83706117a5814174736dbada6fe3f5a0559
Al <alexseier12345@gmail..com>
2021-08-16 19:41:38 +0300

    installed redux devtools

--- my-react-app/src/store/index.js
+++ my-react-app/src/store/index.js
@@ -1,8 +1,9 @@
-import { createStore, combineReducers } from "redux";
-import { switcherReducer } from "./themeSwitcher";
+import { createStore, combineReducers } from "redux"
+import { switcherReducer } from "./themeSwitcher"
 
 export const store = createStore(
-    combineReducers({
-        themeSwitcher: switcherReducer,
-    })
-);
+  combineReducers({
+    themeSwitcher: switcherReducer,
+  }),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+)
