915a45cb9eb28b395157ac88fdfa9f59b1b37a4a
Al <alexseier12345@gmail..com>
2021-08-18 23:12:55 +0300

    deleted message-provider

--- my-react-app/src/pages/chat/Chat.js
+++ my-react-app/src/pages/chat/Chat.js
@@ -1,5 +1,5 @@
-import {Switch, Route, useRouteMatch} from "react-router-dom"
-import { Chats, Layout, Messages, MessageProvider } from "../../components"
+import { Switch, Route, useRouteMatch } from "react-router-dom"
+import { Chats, Layout, Messages } from "../../components"
 import style from "./chat.module.scss"
 
 export const Chat = () => {
@@ -8,22 +8,16 @@ export const Chat = () => {
   return (
     <Switch>
       <Route path={[`${match.path}/:chatId`, `${match.path}/`]}>
-      <MessageProvider>
-        {([state, actions]) => {
-          return (
-            <Layout chats={<Chats {...state} />}>
-              <Route exact={true} path={`${match.path}`}>
-                <div className={style.prompt}>
-                  <h4 className={style.promptText}>Select a chat!</h4>
-                </div>
-              </Route>
-              <Route path={`${match.path}/:chatId`}>
-                <Messages {...state} {...actions} />
-              </Route>
-            </Layout>
-          )
-        }}
-      </MessageProvider>
+        <Layout chats={<Chats />}>
+          <Route exact={true} path={`${match.path}`}>
+            <div className={style.prompt}>
+              <h4 className={style.promptText}>Select a chat!</h4>
+            </div>
+          </Route>
+          <Route path={`${match.path}/:chatId`}>
+            <Messages />
+          </Route>
+        </Layout>
       </Route>
     </Switch>
   )
