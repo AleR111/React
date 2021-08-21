dc5f5d4eecb70ae06d36bc4c098a834e31e279bc
Al <alexseier12345@gmail..com>
2021-08-21 10:33:42 +0300

    persist connected

--- my-react-app/src/store/index.js
+++ my-react-app/src/store/index.js
@@ -1,16 +1,29 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux"
+import { persistStore, persistReducer } from "redux-persist"
+import storage from "redux-persist/lib/storage"
 import thunk from "redux-thunk"
 import { conversationsReducer } from "./conversations"
 import { messagesReducer } from "./messages"
 import { botAnswer } from "./middlewares/botAnswer"
 import { switcherReducer } from "./themeSwitcher"
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['themeSwitcher']
+}
+
+const rootPersistReducer = persistReducer(
+    persistConfig,
+    combineReducers({
+        themeSwitcher: switcherReducer,
+        conversationsStore: conversationsReducer,
+        messagesStore: messagesReducer,
+    }),
+)
+
 export const store = createStore(
-  combineReducers({
-    themeSwitcher: switcherReducer,
-    conversationsStore: conversationsReducer,
-    messagesStore: messagesReducer,
-  }),
+    rootPersistReducer,
   compose(
     applyMiddleware(botAnswer, thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__
@@ -18,3 +31,5 @@ export const store = createStore(
       : (args) => args,
   ),
 )
+
+export const rootPersistStore = persistStore(store)
