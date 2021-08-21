4ab3f77c5c67d69adccb3c05c0a3098959a9e662
Al <alexseier12345@gmail..com>
2021-08-21 11:53:18 +0300

    made popover outside component of chats

---
+++ my-react-app/src/components/chats/popover/PopoverComp.js
@@ -0,0 +1,51 @@
+import { Popover, Paper, Button } from "@material-ui/core"
+import { Delete } from "@material-ui/icons"
+import { useDispatch } from "react-redux"
+import { useHistory } from "react-router-dom"
+import { deleteConversation } from "../../../store/conversations"
+import { deleteConversationMessages } from "../../../store/messages"
+
+export const PopoverComp = ({
+  anchorEl,
+  setAnchorEl,
+  contextChatId,
+  chatId,
+}) => {
+  const history = useHistory()
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  const open = Boolean(anchorEl)
+
+  const dispatch = useDispatch()
+
+  const deleteChat = () => {
+    dispatch(deleteConversation(contextChatId))
+    dispatch(deleteConversationMessages(contextChatId))
+    handleClose()
+    if (contextChatId === chatId) history.push("/chat")
+  }
+
+  return (
+    <Popover
+      open={open}
+      anchorEl={anchorEl}
+      onClose={handleClose}
+      anchorOrigin={{
+        vertical: "center",
+        horizontal: "center",
+      }}
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "left",
+      }}
+    >
+      <Paper variant="outlined">
+        <Button onClick={deleteChat} variant="contained" startIcon={<Delete />}>
+          Delete
+        </Button>
+      </Paper>
+    </Popover>
+  )
+}
