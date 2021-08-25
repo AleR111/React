ea85048e7709a3362702e696cd809f6c57d52bb3
Al <alexseier12345@gmail..com>
2021-08-25 23:42:33 +0300

    data display done

--- my-react-app/src/pages/publicGistsApi/PublicGistsApi.js
+++ my-react-app/src/pages/publicGistsApi/PublicGistsApi.js
@@ -1,14 +1,45 @@
-import {useEffect} from "react";
-import {useDispatch} from "react-redux";
-import {getPublicGists} from "../../store/publicGists/thunks";
+import { LinearProgress } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core/styles"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { getPublicGists } from "../../store/publicGists/thunks"
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    "& > * + *": {
+      marginTop: theme.spacing(2),
+    },
+  },
+  error: {
+    color: "red",
+  },
+}))
 
 export const PublicGistsApi = () => {
-    const dispatch = useDispatch()
+  const classes = useStyles()
+  const { data, isPending, error } = useSelector(
+    (store) => store.publicGistsStore,
+  )
+  const dispatch = useDispatch()
 
-    useEffect(() => {
-        dispatch(getPublicGists())
-    },[dispatch])
+  useEffect(() => {
+    dispatch(getPublicGists())
+  }, [dispatch])
 
-return null
+  if (isPending) {
+    return (
+      <div className={classes.root}>
+        <LinearProgress />
+      </div>
+    )
+  }
+
+  if (error) {
+    return <h1 className={classes.error}>{error}</h1>
+  }
+
+  return data.map((elem, index) => {
+    return <div key={index}>{elem.owner.login}</div>
+  })
 }
