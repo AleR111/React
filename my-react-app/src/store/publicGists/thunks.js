554c00f67c6e51e8462ebeb8827e320092275e91
Al <alexseier12345@gmail..com>
2021-08-26 23:51:20 +0300

    update thunk

--- my-react-app/src/store/publicGists/thunks.js
+++ my-react-app/src/store/publicGists/thunks.js
@@ -3,38 +3,29 @@ import { loadingError, loadingStart, loadingSuccess } from "./actions"
 const PUBLIC_GISTS_URL = "https://api.github.com/gists/public"
 const PUBLIC_GISTS_USER_URL = "https://api.github.com/users"
 
+const loadData = async (dispatch, url) => {
+  dispatch(loadingStart())
+  try {
+    const response = await fetch(url)
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`)
+    }
+
+    const data = await response.json()
+    dispatch(loadingSuccess(data))
+  } catch (error) {
+    dispatch(loadingError(error.message))
+  }
+}
+
 export const getPublicGists =
   (page = 1) =>
   async (dispatch) => {
-    dispatch(loadingStart())
-    try {
-      const response = await fetch(`${PUBLIC_GISTS_URL}?page=${page}`)
-      if (!response.ok) {
-        throw new Error(`Error: ${response.status}`)
-      }
-
-      const data = await response.json()
-      dispatch(loadingSuccess(data))
-    } catch (error) {
-      console.log(error.message)
-      dispatch(loadingError(error.message))
-    }
+    const url = `${PUBLIC_GISTS_URL}?page=${page}`
+    await loadData(dispatch, url)
   }
 
-export const searchPublicGistsByLogin =
-    (login) =>
-        async (dispatch) => {
-            dispatch(loadingStart())
-            try {
-                const response = await fetch(`${PUBLIC_GISTS_USER_URL}/${login}`)
-                if (!response.ok) {
-                    throw new Error(`Error: ${response.status}`)
-                }
-
-                const data = await response.json()
-                dispatch(loadingSuccess(data))
-            } catch (error) {
-                console.log(error.message)
-                dispatch(loadingError(error.message))
-            }
-        }
+export const searchPublicGistsByLogin = (login) => async (dispatch) => {
+  const url = `${PUBLIC_GISTS_USER_URL}/${login}`
+  await loadData(dispatch, url)
+}
