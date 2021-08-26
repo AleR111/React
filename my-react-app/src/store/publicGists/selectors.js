bc5ba0a48bceba4511242b93fdf7ce627be5eb4b
AleR111 <71905890+AleR111@users.noreply.github.com>
2021-08-27 00:13:15 +0300

    Delete selectors.js

--- my-react-app/src/store/publicGists/selectors.js
+++
@@ -1,9 +0,0 @@
-export const getConversations = (state) =>
-  state.conversationsStore.conversations
-
-export const getCurrentConversations = (state, chatId) =>
-  state.conversationsStore.conversations.find((elem) => elem.id === chatId)
-
-export const getValue = (state, chatId) =>
-  state.conversationsStore.conversations.find((elem) => elem.id === chatId)
-    ?.value || ""
