cc6b42220b887465b30d882fe7c2c1c4c8e8fe95
Al <alexseier12345@gmail..com>
2021-09-20 19:55:59 +0300

    update app

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,4 +1,4 @@
-import { createTheme, ThemeProvider } from "@material-ui/core/styles"
+import { ThemeProvider } from "@material-ui/core/styles"
 import { useEffect, useMemo } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { Route, Switch } from "react-router-dom"
@@ -8,81 +8,7 @@ import { Chat, Page404, Profile, SignIp, SignUp } from "./pages"
 import { PublicGistsApi } from "./pages/publicGistsApi"
 import { PrivatePage, PublicPage } from "./route"
 import { requestAuth } from "./store/auth/thunks"
-// import { getConversationsFromDB } from "./store/conversations"
-// import { getMessageFromDB } from "./store/messages"
-
-const themes = {
-  dark: createTheme({
-    font: {
-      color: "rgba(255,255,255,0.89)",
-    },
-    background: {
-      color: "#19181f",
-    },
-    fontColor: {
-      color: "rgba(255,255,255,0.89)",
-    },
-    header: {
-      backgroundColor: "#212227",
-    },
-    drawer: {
-      backgroundColor: "rgb(45,48,51)",
-    },
-    burger: {
-      color: "#fff",
-    },
-    chats: {
-      backgroundColor: "#242a37",
-    },
-    messagesList: {
-      backgroundColor: "#19181f",
-      border: "4px solid #09141f",
-    },
-    messagesInput: {
-      backgroundColor: "#353f4b",
-    },
-    userMessage: {
-      backgroundColor: "#2b5278",
-    },
-    companionMessage: {
-      backgroundColor: "#353f4b",
-    },
-  }),
-
-  light: createTheme({
-    font: {
-      color: "rgba(0,0,0,0.89)",
-    },
-    background: {
-      color: "#ffffff",
-    },
-    header: {
-      backgroundColor: "#edf4f6",
-    },
-    drawer: {
-      backgroundColor: "#edf4f6",
-    },
-    burger: {
-      color: "#000",
-    },
-    chats: {
-      backgroundColor: "#d7dad9",
-    },
-    messagesList: {
-      backgroundColor: "rgba(157,190,186,0.47)",
-      border: "4px solid #357b5821",
-    },
-    messagesInput: {
-      backgroundColor: "#f2f8fc",
-    },
-    userMessage: {
-      backgroundColor: "#76ada2",
-    },
-    companionMessage: {
-      backgroundColor: "#7f8db2",
-    },
-  }),
-}
+import {themes} from './themes'
 
 export const App = () => {
   const selectorTheme = useMemo(
@@ -95,13 +21,7 @@ export const App = () => {
   const themeApp = useSelector(selectorTheme)
   const dispatch = useDispatch()
 
-  // useEffect(() => {
-  //   dispatch(getMessageFromDB())
-  //   dispatch(getConversationsFromDB())
-  // }, [dispatch])
-
   useEffect(() => {
-    ////useLayoutEffect чтоб не мигал сначала чат, потом загрузка и снова чат при перезагрузке страницы
     dispatch(requestAuth())
   }, [dispatch])
 
