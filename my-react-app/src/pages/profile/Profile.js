7bb101a224301c5ec55a4269219fdf63299df0bf
Al <alexseier12345@gmail..com>
2021-09-21 20:35:42 +0300

    start creating profile

--- my-react-app/src/pages/profile/Profile.js
+++ my-react-app/src/pages/profile/Profile.js
@@ -1,5 +1,34 @@
+import { Container, Avatar} from "@material-ui/core"
+import { makeStyles } from "@material-ui/core/styles"
+import style from "./profile.module.scss"
+
+const useStyle = makeStyles((theme) => ({
+    root: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: '100%'
+    },
+  profilePage: {
+    backgroundColor: theme.background.color,
+    height: "100%",
+  },
+    large: {
+        width: theme.spacing(10),
+        height: theme.spacing(10),
+    },
+}))
+
 export const Profile = () => {
-    return (
-        <h1>There will be a page of some profile</h1>
-    )
+  const classes = useStyle()
+  return (
+    <div className={classes.profilePage}>
+      <Container maxWidth="sm" className={classes.root}>
+        <div className={style.box}>
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" className={classes.large} />
+        </div>
+      </Container>
+    </div>
+  )
 }
