dc599353b20724a6cc30f62e5545a96a6f00cd0a
Al <alexseier12345@gmail..com>
2021-09-22 18:21:40 +0300

    to do profile

--- my-react-app/src/pages/profile/Profile.js
+++ my-react-app/src/pages/profile/Profile.js
@@ -1,5 +1,6 @@
-import { Container, Avatar} from "@material-ui/core"
+import { Container, Avatar, Grid, Input, FormHelperText} from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
+import {PersonOutline} from '@material-ui/icons';
 import style from "./profile.module.scss"
 
 const useStyle = makeStyles((theme) => ({
@@ -12,12 +13,17 @@ const useStyle = makeStyles((theme) => ({
     },
   profilePage: {
     backgroundColor: theme.background.color,
+      color: '#fff',
     height: "100%",
   },
     large: {
         width: theme.spacing(10),
         height: theme.spacing(10),
     },
+    icon: {
+        color: theme.font.color,
+        fontSize: '32px'
+    }
 }))
 
 export const Profile = () => {
@@ -25,8 +31,72 @@ export const Profile = () => {
   return (
     <div className={classes.profilePage}>
       <Container maxWidth="sm" className={classes.root}>
+          TODO
         <div className={style.box}>
             <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" className={classes.large} />
+            <Grid container={true} spacing={2} alignItems={"center"} >
+                <Grid item={true}>
+                    <PersonOutline className={classes.icon}/>
+                </Grid>
+                <Grid item={true}>
+                        <Input
+                            id="standard-adornment-weight"
+                            aria-describedby="standard-weight-helper-text"
+                            inputProps={{
+                                'aria-label': 'weight',
+                            }}
+                        />
+                        <FormHelperText id="standard-weight-helper-text">Weight</FormHelperText>
+                </Grid>
+            </Grid>
+            <Grid container={true} spacing={2} alignItems={"center"} >
+                <Grid item={true}>
+                    <PersonOutline className={classes.icon}/>
+                </Grid>
+                <Grid item={true}>
+                    <Input
+                        id="standard-adornment-weight"
+                        aria-describedby="standard-weight-helper-text"
+                        inputProps={{
+                            'aria-label': 'weight',
+                        }}
+                    />
+                    <FormHelperText id="standard-weight-helper-text">Weight</FormHelperText>
+                </Grid>
+
+            </Grid>
+            <Grid container={true} spacing={2} alignItems={"center"} >
+                <Grid item={true}>
+                    <PersonOutline className={classes.icon}/>
+                </Grid>
+                <Grid item={true}>
+                    <Input
+                        id="standard-adornment-weight"
+                        aria-describedby="standard-weight-helper-text"
+                        inputProps={{
+                            'aria-label': 'weight',
+                        }}
+                    />
+                    <FormHelperText id="standard-weight-helper-text">Weight</FormHelperText>
+                </Grid>
+
+            </Grid>
+            <Grid container={true} spacing={2} alignItems={"center"} >
+                <Grid item={true}>
+                    <PersonOutline className={classes.icon}/>
+                </Grid>
+                <Grid item={true}>
+                    <Input
+                        id="standard-adornment-weight"
+                        aria-describedby="standard-weight-helper-text"
+                        inputProps={{
+                            'aria-label': 'weight',
+                        }}
+                    />
+                    <FormHelperText id="standard-weight-helper-text">Weight</FormHelperText>
+                </Grid>
+
+            </Grid>
         </div>
       </Container>
     </div>
